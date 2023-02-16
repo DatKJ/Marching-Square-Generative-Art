b5373486330616fc73a9a5faed599c2a72e07cd2
DatKJ <105619733+DatKJ@users.noreply.github.com>
2023-02-16 21:59:44 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,16 +10,11 @@ var word;
 let frameCount = 0;
 
 function preload(){
-  img = loadImage('DSC_6739.jpg');
-  //word = loadImage('words.png');
+  img = loadImage('image.jpg');  //image file name
 }
 
 function setup() {
-createCanvas(img.width, img.height);
-
-  //createCanvas(457, 609);
-
-  //pixelDensity(1);
+  createCanvas(img.width, img.height);
 
   noise = new OpenSimplexNoise(Date.now());
   cols = 1 + width / rez;
@@ -33,41 +28,16 @@ createCanvas(img.width, img.height);
   }
 
   img.resize(img.width , img.height);
-  // word.resize(width, height);
-  //img.loadPixels;
-  //loadPixels();
-  // for(let x = 0; x < img.width; x++){
-  //   let bright = [];
-  //   for(let y = 0; y < img.height; y++){
-  //     let index = (x + y * width)*4;
-  //     let r = img.pixels[index+0];
-  //     let g = img.pixels[index+1];
-  //     let b = img.pixels[index+2];
-  //
-  //     bright.push((r+g+b)/3);
-  //
-  //
-  //   }
-  //      text.push(bright);
-  // }
 
   img.loadPixels;
   loadPixels();
-  //image(img, 0, 0);
 
   for(let x = 0; x < img.width + rez; x+=rez){
     let bright = [];
     for(let y = 0; y < img.height + rez; y+=rez){
 
       let c = img.get(x,y);
-      //bright[y] = map(brightness(c), 0, 255, 255, 0);
       bright.push(brightness(c));
-
-
-      // fill(c);
-      //stroke(100);
-      // rect(x, y, rez, rez);
-
 
     }
     text.push(bright);
@@ -76,29 +46,11 @@ createCanvas(img.width, img.height);
 
 }
 
-
-
-  // function showText(){
-  //
-  //   for (let i = 0; i < cols; i++) {
-  //     for (let j = 0; j < rows; j++) {
-  //       fill(text[i][j]*255, 100, text[i][j]*255);
-  //       noStroke();
-  //       rect(i*rez, j*rez, rez, rez);
-  //
-  //     }
-  //   }
-  //
-  // }
-
 function drawLine(v1, v2) {
   line(v1.x, v1.y, v2.x, v2.y);
 }
 
 function draw() {
-//background(255);
-//frameCount(60);
-
 
   let xoff = 0;
   for (let i = 0; i < cols; i++) {
@@ -121,8 +73,6 @@ function draw() {
     for (let j = 0; j < rows; j++) {
       fill(field[i][j]*255, 100, field[i][j]*255);
 
-
-      //strokeWeight(0.1);
       noStroke();
       rect(i*rez, j*rez, rez, rez);
 
@@ -198,14 +148,9 @@ function draw() {
   }
 
 //saveFrames('img'+ frameCount, 'jpg');
-//frameCount++;
-  //image(word, 0, 0);
+
  }
 
 function getState(a, b, c, d) {
   return a * 8 + b * 4 + c * 2 + d * 1;
 }
-
-   //function mousePressed(){
-    //saveFrames('##', 'jpg', 10,30);
-   //}
